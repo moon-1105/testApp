5f6fd0e20059dedb5768e6e123689996c1c93b61
문소연 선임 클라우드응용서비스팀 (so_yeon.Moon@lgcns.com <so_yeon.Moon@CNS.LGCNS.COM>
2022-10-18 13:44:27 +0900

    memory 사용 로직 버튼 연결

--- src/main/java/com/mw1/testApp/service/MainService.java
+++ src/main/java/com/mw1/testApp/service/MainService.java
@@ -1,25 +1,40 @@
 package com.mw1.testApp.service;
 
+import groovy.util.logging.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class MainService {
     public void usingMemory(){
+        log.info("Memory TEST start");
         List<byte[]> list = new ArrayList<>();
         int index = 1;
-        int flag = 0;
-        while (flag <= 1000) {
+        while (index <= 1000) {
             // 1MB each loop, 1 x 1024 x 1024 = 1048576
             byte[] b = new byte[1048576];
             list.add(b);
             Runtime rt = Runtime.getRuntime();
-            if (flag%10 == 0){
-                System.out.printf("[%d] free memory: %s%n", index++, rt.freeMemory());
-            }
-            flag++;
+            //자바 가상 머신(JVM)의 모든 메모리 양
+            log.info("[{}] Total memory: {}MB", index, rt.totalMemory() / (1024 * 1024));
+            //가상머신이 사용하려고 시도했던 가장 큰 메모리 양  -Xmx 값과 같다
+            log.info("[{}] Max memory: {}MB", index, rt.maxMemory() / (1024 * 1024));
+            //자바 가상머신 내의 남은 메모리의 양
+            log.info("[{}] Free memory: {}MB", index++, rt.freeMemory() / (1024 * 1024));
+            log.info("============================================");
+             try{
+               Thread.sleep(1000);
+             }catch(InterruptedException e){
+               e.printStackTrace();
+             }
         }
+        return;
+    }
+    public void stopUsingMemory(){
+        return;
     }
 }
